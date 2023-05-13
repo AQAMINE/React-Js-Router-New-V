f217bbd6d5beaf34c7f0018260e86424c3b9292e
aqamine <32686488+AQAMINE@users.noreply.github.com>
2023-05-13 14:33:58 +0100

    relative & absolute path

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,8 +1,10 @@
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 
 export default function ProductDetailPage() {
     const params = useParams();
 
     return <><h1>Products Details!</h1>
-            <p>{params.productId}</p></>
+            <p>{params.productId}</p>
+            <Link to=".." relative="path">Back</Link>
+            </>
 }
